f41892da18beacaf63380fc62c8d121b0d53a648
root <sach2008@outlook.com>
2018-06-27 15:27:33 +0530

    added code for single data view

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
+  state = {
+    loadedData : null
+  }
+  componentDidUpdate (){
+    if(this.props.id){
+      if(!this.state.loadedData || (this.state.loadedData && this.state.loadedData.id !== this.props.id) ){
+      axios.get('https://jsonplaceholder.typicode.com/posts/'+this.props.id)
+      .then(response=>{
+          this.setState({loadedData:response.data});
+      });
+
+    }
+  }
+}
+
+    render (){
         let post = <p>Please select a Post!</p>;
         if(this.props.id){
+          post = <p>Loading!!!!!</p>
+        }
+
+        if(this.state.loadedData){
           post = (
               <div className="FullPost">
-                  <h1>Title</h1>
+                  <h1>{this.state.loadedData.title}</h1>
                   <p>Content</p>
                   <div className="Edit">
                       <button className="Delete">Delete</button>
