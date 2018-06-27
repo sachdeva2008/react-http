24192c2c303db6c63477a7a96c1edb4edd783794
root <sach2008@outlook.com>
2018-06-27 12:55:11 +0530

    parsing recieved data into page

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -4,7 +4,7 @@ import './Post.css';
 
 const post = (props) => (
     <article className="Post">
-        <h1>Title</h1>
+        {props.title}
         <div className="Info">
             <div className="Author">Author</div>
         </div>
