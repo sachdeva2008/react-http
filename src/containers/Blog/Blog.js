4ed12689e5010eef7b93ac1bfaee58e6f68be451
root <sach2008@outlook.com>
2018-06-28 11:20:08 +0530

    installed router package and added lines for it in app.js and deleted import for newpost fullpost and post from blogjs

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+
 import './Blog.css';
 
 class Blog extends Component {
@@ -39,6 +37,14 @@ class Blog extends Component {
       });
         return (
             <div>
+              <header>
+                <nav>
+                  <ul>
+                    <li><a href='/'>Home</a></li>
+                    <li><a href='/new-post'>New Post</a></li>
+                  </ul>
+                </nav>
+              </header>
                 <section className="Posts">
                     {posts}
 
