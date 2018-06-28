1ea9153c02cfc8d9d181f0996244fe4f8a59ced2
root <sach2008@outlook.com>
2018-06-28 11:22:16 +0530

    changed folder structure for these file

---
+++ src/containers/Blog/FullPost/FullPost.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './FullPost.css';
+
+class FullPost extends Component {
+  state = {
+    loadedData : null,
+    error : false
+  }
+  componentDidUpdate (){
+    if(this.props.id){
+      if(!this.state.loadedData || (this.state.loadedData && this.state.loadedData.id !== this.props.id) ){
+      axios.get('https://jsonplaceholder.typicode.com/posts/'+this.props.id)
+      .then(response=>{
+          this.setState({loadedData:response.data});
+      });
+
+    }
+  }
+}
+
+deleteDataHandler = () => {
+  axios.delete('https://jsonplaceholder.typicode.com/posts/'+this.props.id)
+  .then(response=>{
+      console.log(response);
+  });
+}
+
+
+
+
+    render (){
+        let post = <p>Please select a Post!</p>;
+        if(!this.state.error){
+          <p>Something went wrong</p>
+        }
+        if(this.props.id){
+          post = <p>Loading!!!!!</p>
+        }
+
+        if(this.state.loadedData){
+          post = (
+              <div className="FullPost">
+                  <h1>{this.state.loadedData.title}</h1>
+                  <p>{this.state.loadedData.body}</p>
+                  <div className="Edit">
+                      <button onClick={this.deleteDataHandler} className="Delete">Delete</button>
+                  </div>
+              </div>
+
+          );
+        }
+
+        return post;
+    }
+}
+
+export default FullPost;
