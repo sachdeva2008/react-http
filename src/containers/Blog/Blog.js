789b41f482f8f58653e6fb0df8816b6d514de25c
root <sach2008@outlook.com>
2018-06-29 15:07:55 +0530

    Added switch concept for handling routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-import {Route,NavLink} from 'react-router-dom';
+import {Route,NavLink,Switch} from 'react-router-dom';
 
 import './Blog.css';
 
@@ -25,9 +25,11 @@ class Blog extends Component {
                 </nav>
               </header>
               {/*<Route path='/' exact render={() => <h1>Home</h1>} />*/}
-              <Route path='/' exact component={Posts}/>
-              <Route path='/new-post' exact component={NewPost}/>
-              <Route path='/:id' exact component={FullPost}/>
+              <Switch>
+                <Route path='/' exact component={Posts}/>
+                <Route path='/new-post' exact component={NewPost}/>
+                <Route path='/:id' exact component={FullPost}/>
+              </Switch>
             </div>
         );
     }
